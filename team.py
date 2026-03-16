2e226670c673565f72d3adbba369f8cb96862456
arpit-sigmoid <arpit.aggarwal@sigmoidanalytics.com>
2026-03-16 23:36:40 +0530

    main changes

---
+++ team.py
@@ -0,0 +1,11 @@
+#Arpit TSV-886
+def greet():
+    print("HI, I am Arpit Aggarwal  and this is what i am changing TSV-886 wowwo")
+
+
+def farewell():
+    print("Bye, Have a good day and see you later  wow you are gere")
+greet()
+farewell()
+
+
